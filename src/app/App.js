f1dd44d9507d104644b975609124ab6d83c2448a
simon_bellucci <simon.bellucci@adeliom.com>
2020-03-24 16:27:14 +0100

    App Styles

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,20 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      {/* PLOP_MODIFY_ROUTER */}
+    <div className="app">
+
+        <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+            <div style={{fontSize: 90, fontFamily: 'Nunito', fontWeight: 200}}>
+                React Starter CLI
+            </div>
+            <div style={{fontSize: 20, fontFamily: 'Nunito', fontWeight: 200, textAlign: 'center'}}>
+                <p>Type <code style={{background: '#eee', borderRadius: 3, padding: 3, fontSize: 15, color: '#FF4655'}} >yarn generate page page_name</code> to generate a page</p>
+                <p>or <code style={{background: '#eee', borderRadius: 3, padding: 3, fontSize: 15, color: '#FF4655'}} >yarn generate component component_name</code> to generate a component</p>
+            </div>
+        </div>
+
+        {/* PLOP_MODIFY_ROUTER */}
+        
     </div>
   );
 }
